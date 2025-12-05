38db94561df2650581b7effc89f9225f74d51dff
Arham Malik <125215131+Arham2005@users.noreply.github.com>
2025-12-06 01:26:15 +0500

    Add build folder

---
+++ assets/js/fe269f57.2f61f2b9.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkbook_project=globalThis.webpackChunkbook_project||[]).push([[3045],{6781:o=>{o.exports=JSON.parse('{"metadata":{"permalink":"/GIAIC-Hackathon/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
